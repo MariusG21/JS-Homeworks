30f88c7737c8edeb155b854751c67ef57f4531bc
MariusG21 <gheoneamarius10@gmail.com>
2025-05-29 09:59:44 +0300

    adding default data and rendering voting options

---
+++ 03-homework/scripts/data.js
@@ -0,0 +1,26 @@
+const superpowers = [
+  {
+    id: "11",
+    name: "invisibility",
+  },
+  {
+    id: "22",
+    name: "super strength",
+  },
+  {
+    id: "33",
+    name: "reading minds",
+  },
+  {
+    id: "44",
+    name: "time travel",
+  },
+  {
+    id: "55",
+    name: "teleportation",
+  },
+  {
+    id: "66",
+    name: "immortality",
+  },
+];
--- 03-homework/scripts/index.js
+++ 03-homework/scripts/index.js
@@ -1,1 +1,21 @@
-console.log("Hello.. 😊");
+import { superpowers } from "./data.js";
+
+function renderVotingOptions() {
+  let html = "";
+
+  superpowers.forEach((superpower) => {
+    html += `
+      <div class="main__voting-container__voting-option" data-id=${superpower.id}>
+        <div class="illustrative-pic"></div>
+        <div class="superpower-name">${superpower.name}</div>
+        <div class="separator-line"></div>
+        <div class="vote-counter">Votes: <span class="counter">0</span></div>
+        <button class="vote-btn">Vote</button>
+      </div>
+    `;
+  });
+
+  document.querySelector(".main__voting-container").innerHTML = html;
+}
+
+renderVotingOptions();
