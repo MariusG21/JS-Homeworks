cdbbffff17e2908b19a3b91639bf83edca7ba9d6
MariusG21 <gheoneamarius10@gmail.com>
2025-05-29 18:33:50 +0300

    adding event listener on buttons

--- 03-homework/scripts/index.js
+++ 03-homework/scripts/index.js
@@ -12,17 +12,37 @@ function renderVotingOptions(superpowers) {
 
   superpowers.forEach((superpower) => {
     html += `
-      <div class="main__voting-container__voting-option" data-id=${superpower.id}>
+      <div class="main__voting-container__voting-option main__voting-container__voting-option-${superpower.id}">
         <div class="illustrative-pic"></div>
         <div class="superpower-name">${superpower.name}</div>
         <div class="separator-line"></div>
-        <div class="vote-counter">Votes: <span class="counter">${superpower.votes}</span></div>
-        <button class="vote-btn">Vote</button>
+        <div class="vote-counter">Votes: <span class="counter counter-${superpower.id}">${superpower.votes}</span></div>
+        <button class="vote-btn" data-id=${superpower.id}>Vote</button>
       </div>
     `;
   });
 
   document.querySelector(".main__voting-container").innerHTML = html;
+
+  document.querySelectorAll(".vote-btn").forEach((btn) => {
+    btn.addEventListener("click", async () => {
+      btn.disabled = true;
+      const { id } = btn.dataset;
+      const counter = document.querySelector(`.counter-${id}`);
+      const res = await fetch(
+        `https://api.api-ninjas.com/v1/counter?id=${id}&hit=true`,
+        {
+          headers: {
+            "x-api-key": "lapX9tiSjA6BpVn/4v7Mow==GNJcGloReV0TXiRk",
+          },
+        }
+      );
+      const data = await res.json();
+      const newVotes = data.value;
+      counter.textContent = newVotes;
+      btn.disabled = false;
+    });
+  });
 }
 
 renderVotingOptions(superpowers);
